429eb3f37f863781e69d7cc6d685f800e064d3b8
rohithpala <63358944+rohithpala@users.noreply.github.com>
2022-01-20 14:43:00 +0530

    Authenticating User using React Login SignUp Form

--- authentication/src/App.js
+++ authentication/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import LeftPanel from "./components/left-panel";
--- authentication/src/components/left-panel.js
+++ authentication/src/components/left-panel.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../stylesheets/left-panel.css";
 
 class LeftPanel extends Component {
