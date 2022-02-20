8f085a2f947834ba3037428016742ccb12619622
rohithpala <rohithpala02@gmail.com>
2022-02-20 21:13:50 +0530

    Accordions in ReactJS

---
+++ accordion/src/accordion.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import "./accordion.css";
+
+class Accordion extends Component {
+   toggleAccordion = (e) => { // TODO
+      console.log(e);
+      const content = e.nextElementSibling;// document.querySelector(".content");
+      if (content.style.maxHeight) content.style.maxHeight = null;
+      else content.style.maxHeight = content.scrollHeight + "px";
+   }
+   render() {
+      return (
+         <React.Fragment>
+            <div className="accordion" onClick={() => this.toggleAccordion()}>
+               Accordion {this.props.accordionName}
+            </div>
+            <div className="content">
+               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer blandit, tellus non posuere pretium, lorem libero gravida leo, id tempor metus sapien non nibh. Cras
+            </div>
+         </React.Fragment>
+      );
+   }
+}
+
+export default Accordion;
