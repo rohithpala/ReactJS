89800a8e570c12b569e17b61647d67091a100e5b
rohithpala <rohithpala02@gmail.com>
2022-03-20 19:41:19 +0530

    Task Management App

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./stylesheets/App.css";
+
+import Task from "./components/task";
+import AddTaskModal from "./components/addTaskModal";
+
+class App extends Component {
+  state = {
+    addTaskModal: true
+  }
+
+  handleAddTask = () => {
+    React.render(<AddTaskModal />, document.getElementById("root"));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="top-container">
+          <button className="add-btn" onClick={this.handleAddTask}>
+            Add New Task <FontAwesomeIcon icon={faPlus} />
+          </button>
+          <a href="https://github.com/rohithpala/ReactJS/tree/main/task-app" target="_blank" rel="noreferrer">
+            <div className="code">Code</div>
+          </a>
+        </div>
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+        <Task />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ task-app/src/components/task.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrashCan } from "@fortawesome/free-solid-svg-icons";
+
+import "../stylesheets/task.css";
+
+class Task extends Component {
+
+   handleEdit = () => {
+      
+   }
+
+   handleDelete = () => {
+      
+   }
+
+   render() {
+      return (
+         <div className="task">
+            <span className="task-content">Task</span>
+            <div className="btn-container">
+               <button className="icon edit-btn" onClick={this.handleEdit}><FontAwesomeIcon icon={faEdit} /></button>
+               <button className="icon delete-btn" onClick={this.handleDelete}><FontAwesomeIcon icon={faTrashCan} /></button>
+            </div>
+         </div>
+      );
+   }
+}
+
+export default Task;
